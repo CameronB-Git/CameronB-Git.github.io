35b9dc74daf5a0e2d431530bcc26840c85bd20e2
Cameron Branston <cammybro11@gmail.com>
2024-03-22 18:57:59 +0000

    Minor Changes
    General -
     - Deleted About Page --> About button is now linked to reckitt about
     - Optimised some efficiency in scripts
    
    Homepage -
     - Added Alt + Title tags on all brand images
    
    Products.json
     - Added Hazards + Cautions to all products
     - Finished Strepsils --> how to use
    
    Script.js + basket.html
     - Added Remove Button functionality
     - Ensured Add to Basket works properly
    
    Styling.css -
     - Product page dropdown menu is now responsive to mobile devices
    
    Styling.css (PORTFOLIO) -
     - Added white borders around invoice dashboard and reckitt retail images.

--- Assets/sites/reckittSite/src/script.js
+++ Assets/sites/reckittSite/src/script.js
@@ -5,8 +5,9 @@ var items = [];
 let selectedProduct;
 let firstItemId;
 let totalPrice = 0;
-let productsInBasket = [];
 let unNeededPages = ["index.html", "basket.html", "checkout.html", "confirmation.html", "billingInformation.html"];
+let ibuprofenContaining = [9, 10, 11, 12, 13];
+let paracetamolContaining = [14, 15, 16, 17, 18];
 
 document.addEventListener("DOMContentLoaded", function() {
     // Load product data from JSON
@@ -39,13 +40,14 @@ document.addEventListener("DOMContentLoaded", function() {
             if (selectedProductIndex === undefined) {
                 selectedProductIndex = firstItemId;
             }
-            
+
             // Call a function to add the product to the basket, passing the selected product ID as a parameter
             addItemToBasket(selectedProductIndex);
             
         });
     }
 });
+
 
 // Function to load product information based on selected product index
 function loadProductInfo(selectedProductIndex) {
@@ -64,41 +66,74 @@ function loadProductInfo(selectedProductIndex) {
     
         // Update product image, name, price, and other relevant information
         document.getElementById('productImage').src = selectedProduct.imageLocation;
+        document.getElementById('productImage').alt = `${selectedProduct.name} (Product) Image`; // Set Alt Text for Image
+        document.getElementById('productImage').title = selectedProduct.name; // Set Title for Product Name
         document.getElementById('productName').innerText = selectedProduct.name;
-        document.getElementById('productPrice').innerText = '£' + selectedProduct.price.toFixed(2);
+        document.getElementById('productPrice').innerText = '£' + selectedProduct.price.toFixed(2); // Set Price to 2 Decimal Places
         document.getElementById('howToUse').innerHTML = selectedProduct.howToUse;
         document.getElementById('productPIL').href = selectedProduct.pilLocation;
+        document.getElementById('hazardsCautions').innerHTML = selectedProduct.hazardsCautions;
+        
     
         // If product has a 2nd/3rd how to use, display it
         if (selectedProduct.howToUseTwo != undefined) {
             document.getElementById('howToUseTwo').innerHTML = selectedProduct.howToUseTwo;
         }
         else{
-            document.getElementById('howToUseTwo').innerHTML = '';
+            document.getElementById('howToUseTwo').innerHTML = "";
         }
     
         if(selectedProduct.howToUseThree != undefined) {
             document.getElementById('howToUseThree').innerHTML = selectedProduct.howToUseThree;
         }
         else{
-            document.getElementById('howToUseThree').innerHTML = '';
+            document.getElementById('howToUseThree').innerHTML = "";
         }
     }
 }
 
 function addItemToBasket() {
-    if (selectedProductIndex === undefined) {
-        console.error("No product selected.");
-    } 
-    else {
-        productsInBasket = JSON.parse(localStorage.getItem("item")) || [];
-    
-        productsInBasket.push(selectedProductIndex);
-        localStorage.setItem("item", JSON.stringify(productsInBasket));
+    // Retrieve current basket items from local storage or initialize an empty array
+    let basketItems = JSON.parse(localStorage.getItem('basket')) || [];
 
-        selectedProduct = products[selectedProductIndex];
-        alert(`Added ${selectedProduct.name} to your basket!`)
-    }
+    // Setting the Item to Add as the 'selectedProductIndex'
+    let item = selectedProductIndex; 
+    item = parseInt(item);
+
+    // Set Number of Ibuprofen/Paracetamol Products Found to 0
+    var ibuprofenProductsFound = 0;
+    var paracetamolProductsFound = 0;
+
+    if (ibuprofenContaining.includes(item)){
+        for (let i = 0; i < basketItems.length; i++){
+            if (ibuprofenContaining.includes(basketItems[i])){
+                ibuprofenProductsFound++;
+                if (ibuprofenProductsFound >= 2){
+                    alert("You cannot add more than two Ibuprofen products to your basket.");
+                    return;
+                }  
+            }
+        }
+    }  
+    
+    if (paracetamolContaining.includes(item)){
+        for (let i = 0; i < basketItems.length; i++){
+            if (paracetamolContaining.includes(basketItems[i])){
+                paracetamolProductsFound++;
+                if (paracetamolProductsFound >= 2){
+                    alert("You cannot add more than two Paracetamol products to your basket.");
+                    return;
+                }  
+            }
+        }
+    }  
+
+    // Update Basket --> Adds index of selected product to the basket
+    basketItems.push(item);
+
+    // Store the updated basket in local storage
+    localStorage.setItem('basket', JSON.stringify(basketItems));
+    alert(`Added ${selectedProduct.name} to your basket!`)
 }
 
 function displayProductsInBasket() {
@@ -107,13 +142,14 @@ function displayProductsInBasket() {
     // If selectedProductInded = Array AND contains >1 product, loop through each.
     if (Array.isArray(selectedProductIndex) && selectedProductIndex.length > 0) {
         for (let i = 0; i < selectedProductIndex.length; i++) {
+
             // Creating Div (Product Box)
             var target = document.querySelector('#productHeader');
             var div = document.createElement('div');
             div.className = 'container-fluid productBox';
             div.id = `div${i}`;
             target.parentNode.insertBefore(div, target.nextSibling);
-
+            
             // Create Row
             var target = document.querySelector('.container-fluid.productBox');
             var divRow = document.createElement('div');
@@ -122,7 +158,7 @@ function displayProductsInBasket() {
             
             selectedProduct = products[selectedProductIndex[i]]; // Get the selected product
 
-            // If Product Name Contains "Capsules" --> Make Image Column Larger 
+            // Set Column Widths --> Helps with Responsiveness
             if ((selectedProduct.name).includes("Capsules")){
                 imageColumn = 'col-sm-3';
                 textColumn = 'col-sm-9';
@@ -172,19 +208,22 @@ function displayProductsInBasket() {
             removeButton.className = 'removeButton d-inline';
             removeButton.href = "#";
             removeButton.textContent = 'Remove';
-            removeButton.id = div.id;
-            //removeButton.onclick = removeItem(removeButton.id);
+            removeButton.id = selectedProductIndex[i];
+            removeButton.onclick = function() {
+                removeParentElement(selectedProductIndex);
+            };
             target.appendChild(removeButton);  
 
             totalPrice = totalPrice + (parseFloat(selectedProduct.price));
         }
 
-        // Add Subtotal Information 
+        // Add Subtotal Container 
         var target = document.querySelector('#div0')
         var subtotalDiv = document.createElement('div');
         subtotalDiv.className = 'container-fluid subtotalContainer';
         target.parentNode.insertBefore(subtotalDiv, target.nextSibling);
 
+        // Add Subtotal Heading + Value
         var subtotalHeading = document.createElement('h5');
         subtotalHeading.className = 'normalFont';
         subtotalHeading.textContent = `Subtotal: £${totalPrice.toFixed(2)}`;
@@ -195,7 +234,7 @@ function displayProductsInBasket() {
         checkoutButton.className = 'btn btn-secondary buttonTemplate mb-5';
         checkoutButton.innerHTML = "<b>Checkout Now</b>";
         checkoutButton.onclick = function() {
-            window.location.href = "/checkout.html";
+            window.location.href = "/Assets/sites/reckittSite/checkout.html";
         }
         subtotalDiv.appendChild(checkoutButton)
     } else {
@@ -206,16 +245,46 @@ function displayProductsInBasket() {
         h2.textContent = "You currently have no items in your basket.";
         target.parentNode.insertBefore(h2, target.nextSibling);
     }
+
+    // If < 2 Products in Basket, Footer is Fixed to Bottom
+    if (selectedProductIndex.length < 2){
+        footer = document.querySelector('footer');
+        footer.style.position = 'fixed';
+    }
 }
 
 function getItemIndex() {
-    var itemIndex = localStorage.getItem("item");
+    var itemIndex = localStorage.getItem("basket");
 
     // Parse the JSON string into an array
     return JSON.parse(itemIndex);
 }
 
-function removeItem(id){
-    var itemToRemove = document.querySelector(`div${id}`)
-    itemToRemove.remove();
+function removeParentElement() {
+    removeButton = document.querySelector('.removeButton');
+    // Get the selected product index associated with the clicked remove button
+    let itemIndex = parseInt(removeButton.id);
+
+    // Retrieve current basket items from local storage or initialize an empty array
+    let basketItems = JSON.parse(localStorage.getItem('basket'));
+
+    // Find the index of the item to remove in the basket array
+    let indexToRemove = basketItems.indexOf(itemIndex);
+
+    // Check if the item exists in the basket array
+    if (indexToRemove !== -1) {
+        // Remove the item from the basket array
+        basketItems.splice(indexToRemove, 1);
+
+        // Store the updated basket in local storage
+        localStorage.setItem('basket', JSON.stringify(basketItems));
+    }
+
+    // Get the parent element of the remove button
+    var parentElement = removeButton.parentNode.parentNode.parentNode;
+    
+    // Remove the parent element
+    parentElement.parentNode.removeChild(parentElement);
+
+    window.location.reload(); // Reload the page to reflect the changes
 }
